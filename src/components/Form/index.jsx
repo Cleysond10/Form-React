6380e9985a97e23b5c271ec9fec5615b7c5d60f5
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-02-24 17:05:08 -0300

    added .css file

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import {
+  Form, Row, Col, Button,
+} from 'react-bootstrap';
+import './Form.css';
 
 export default function index() {
   return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>E-mail</Form.Label>
-        <Form.Control type="email" placeholder="...@gmail.com" />
-        <Form.Text className="text-muted" />
-      </Form.Group>
+    <Form className="form">
+      <Row lg={9} xl={12}>
+        <Col>
+          <Form.Group controlId="formBasicName">
+            <Form.Label><b>Nome:</b></Form.Label>
+            <Form.Control type="name" placeholder="Nome Completo" />
+            <Form.Text className="text-muted" />
+          </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Senha</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Lembre-me" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label><b>E-mail:</b></Form.Label>
+            <Form.Control type="email" placeholder="nome.completo@gmail.com" />
+            <Form.Text className="text-muted" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label><b>Senha:</b></Form.Label>
+            <Form.Control type="password" placeholder=". . . . ." />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Lembre-me" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Salvar
+          </Button>
+        </Col>
+      </Row>
     </Form>
   );
 }
